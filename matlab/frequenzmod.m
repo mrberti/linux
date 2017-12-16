3c66aec78acee0bf2c408b08e3a4a335275e9a05
Simon Bertling <simon.bertling@gmx.de>
2017-12-16 23:55:33 +0900

    improved fm script alot

--- matlab/frequenzmod.m
+++ matlab/frequenzmod.m
@@ -1,53 +1,57 @@
 clear all;
 close all;
 
-disp('Frequenzmodulation');
-
-T_max = 1;
-numsteps = 1000;
-T_delta = T_max / numsteps;
-t = [0:T_delta:T_max];
-
-A = 1;
-f_0 = 3;
-d = 0.1; %/(2*pi);
-
-phi_0 = pi;
-
-
-F = 10;
-s = sin(t*2*pi*F);
-
-s = ones(1,length(t));
-s = randi(2, 1, length(t)) - 1;
-
-%s = [ones(floor(length(t)/2), 1)' 2*ones(ceil(length(t)/2), 1)'] 
-
-u = [];
-for i = [1:length(t)]
-    ui = A * cos(f_0*2*pi*t(i) + d*sum(s([1:i])) + phi_0);
-    u = [u,ui]; % * cumsum(t([0:i])));
+try
+  pkg load control;
 end
 
-w = 2*pi*f_0 + d*s;
+Tend = 1;
+Fs = 500;
+Ts = 1/Fs;
+t = 0:Ts:Tend-Ts;
+N = length(t);
 
-length(t)
-length(u)
+A = 1;
+Fc = 50;
+Fd = 5;
+phi_0 = 0;
 
-filt = [1 1 1 -1 -1];
-filt = filt/length(filt);
+Fsig = 1;
+s = (2*sin(2*pi*Fsig*t) + 0*sin(2*pi*10*Fsig*t))/Fs;
+%s = linspace(0,2,N)/N;
 
-z = conv(u, filt);
-figure('Name','u');
-plot(t,u,'r');
-hold
-%stem(t, u);
-figure('Name','z');
-plot(z);
-figure('Name','w');
-plot(t, w);
-figure('Name','fft');
-plot(abs(fft(filt)));
-freqz(filt);
+F_data = 10; Fsig = F_data;
+data = [-1 1 -1 1 1 -1 1 1 -1 -1 1];
+N_data = length(data);
+o = ones(1,ceil(Fs/F_data))/Fs;
+s = reshape(o'*data,1,[]);
+s = s(1:N);
 
+arg = 2*pi*(Fc*t+Fd*cumsum(s)) + phi_0;
+u = A * cos(arg);
+U = fftshift(fft(u)/N);
 
+f_inst = diff(arg)*Fs/2/pi;
+
+df = 1/Ts/N;
+f = -Fs/2:df:Fs/2-df;
+
+G = G = zpk([0],[-1/(2*pi*Fsig*0.02) -1/(2*pi*0.01)],1);
+y = lsim(G,abs(cumsum(u)),t);
+
+subplot(3,1,1);
+plot(t,u);
+subplot(3,1,2);
+plot(t(1:end-1),f_inst);
+subplot(3,1,3);
+plot(f,abs(U));
+%hold on;
+%plot(f,real(U),'r--');
+%plot(f,imag(U),'g--');
+%figure('Name','w');
+%plot(t, w);
+%figure('Name','fft');
+figure;
+plot(t,y);
+hold on;
+plot(t,s);
